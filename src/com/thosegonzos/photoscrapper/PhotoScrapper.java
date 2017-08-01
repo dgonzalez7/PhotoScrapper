8c055c582f076549dcbdb5efa7a4c20bbdff4fee
Dave Gonzalez <dgonzalez@thosegonzos.com>
2017-07-31 23:29:46 -0500

    Complete - Gets Matt & Becca's wedding photos

--- src/com/thosegonzos/photoscrapper/PhotoScrapper.java
+++ src/com/thosegonzos/photoscrapper/PhotoScrapper.java
@@ -1,10 +1,12 @@
 package com.thosegonzos.photoscrapper;
 
 import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -16,27 +18,24 @@ public class PhotoScrapper
 	public static void main(String[] args) 
 	{
 		String targetUrl = "http://elizabethgreve.com/blog/becca-matt-wedding-at-the-abbington-in-glen-ellyn/";
-		String title = "";
 		String outfile = "MattBeccaHTML.txt";
+		String outFolder = "images";
 		
-		System.out.println("Hello Scrapper!");
-		
-        // JSoup Example 2 - Reading HTML page from URL
         Document doc = null;
+        
+		System.out.println("Hello Scrapper!");
 
+		// Get the webpage document (HTML)
         try 
         {
             doc = Jsoup.connect(targetUrl).get();
-            title = doc.title();
         } 
         catch (IOException e) 
         {
             e.printStackTrace();
         }
         
-        System.out.println("Jsoup Can read HTML page from URL, title : " + title);
-        // System.out.println("\n" + doc.html());
-        
+        // Save webpage document (HTML) into a file
         try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outfile)))
         {
         	writer.write(doc.html());
@@ -45,22 +44,102 @@ public class PhotoScrapper
         {
 			e.printStackTrace();
 		}
-        
-//        Element content = doc.getElementById("content");
-//        Elements paras = content.getElementsByAttribute("img");
-//        for (Element p : paras)
-//        {
-//        	String imgLink = p.attr("src");
-//        	System.out.println(imgLink);
-//        }
-        
+
+        // Parse and grab all image URLs
+        int i = 0;
         Elements media = doc.select("[data-lazyload-src]");
         System.out.println("Media: " + media.size());
+		String[] jpgs = new String[media.size()];
         for (Element src : media)
         {
-
-    		System.out.println(">" + src.tagName() + " , " + src.attr("abs:data-lazyload-src"));
-    		
+    		jpgs[i] = src.attr("abs:data-lazyload-src");
+    		i++;
         }
+        
+        // Arrays.stream(jpgs).forEach(System.out::println);
+
+        // Loop through all images
+		System.out.println("\n\n");
+        for (String jpg : jpgs)
+        {
+			// Get photo URL
+        	int c1 = jpg.indexOf('(');
+			int c2 = jpg.indexOf(')');
+			String newJpg = null;
+			if (c1 != -1)
+			{
+				newJpg = removeChar(jpg, c1, c2);
+			}
+			else
+			{
+				newJpg = jpg;
+			}
+			System.out.println(newJpg);
+			
+			// Get filename used to save photo
+			String jpgFile = getFilename(jpg);
+        	c1 = jpgFile.indexOf('(');
+			c2 = jpgFile.indexOf(')');
+			String newJpgFile = null;
+			if (c1 != -1)
+			{
+				newJpgFile = removeChar(jpgFile, c1, c2);
+			}
+			else
+			{
+				newJpgFile = jpgFile;
+			}
+			System.out.println("> "+ newJpgFile);
+			
+			// Get photo at URL...
+	        Response resultImageResponse = null;
+			try 
+			{
+				resultImageResponse = Jsoup.connect(newJpg)
+				        .ignoreContentType(true).execute();
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+	        
+			// ...and save it at filename
+	        FileOutputStream out = null;
+			try 
+			{
+				out = (new FileOutputStream(new java.io.File(outFolder + "/" + newJpgFile)));
+				out.write(resultImageResponse.bodyAsBytes());
+				out.close();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+        }
+        
+	}
+	
+	// Removes a substring from inside a String
+	public static String removeChar(String str, int c1, int c2) 
+	{
+		String front = str.substring(0, c1);
+		String back = str.substring(c2+1, str.length());
+		return front + back;
+	}
+	
+	// Extracts the filename from a URL
+	public static String getFilename(String str) 
+	{
+		int last = str.lastIndexOf('/');
+		String back = str.substring(last+1, str.length());
+		return back;
 	}
 }
+
+
+
+
+
+
+
+
+
